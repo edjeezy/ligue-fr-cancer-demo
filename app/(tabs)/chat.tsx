8ecdcdb12ee7460de96da30299fcde88778325ca
Mouhamed Edouard Junior Mar <mouhamededji@gmail.com>
2024-08-20 18:32:29 +0000

    added chatpage

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,5 +1,37 @@
-import { ThemedText } from "@/components/ThemedText";
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat, IMessage } from 'react-native-gifted-chat'
 
 export default function ChatScreen() {
-    return (<ThemedText type="title">Hello World</ThemedText>);
+  const [messages, setMessages] = useState<IMessage[] | undefined>([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages: IMessage[] = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
